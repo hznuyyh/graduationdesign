7990540aef6dc700a9d00febb7ed605c6f83969e
yeyuhang <yeyuhang@mifengkong.cn>
2017-10-03 23:12:50 +0800

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('explore')->insert([
+            'title' => str_random(10),
+            'content' => str_random(200),
+            'user_id' => random_int(1,100),
+            'topic_id' => random_int(1,100),
+            'comments_count' => random_int(1,100),
+            'goods_count' => random_int(1,100),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
     }
 }

28db5e74cd8e5b32e1704cdc3d643e228898eed6
hznuyyh <623936780@qq.com>
2017-10-21 22:24:24 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,7 +96,7 @@
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/hznuyyh">GitHub</a>
                 </div>
             </div>
         </div>

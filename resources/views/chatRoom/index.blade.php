ee46800304edead8c01416c1568546429d73ec9e
hznuyyh <623936780@qq.com>
2017-12-05 18:30:20 +0800

    update

--- resources/views/chatRoom/index.blade.php
+++ resources/views/chatRoom/index.blade.php
@@ -38,7 +38,7 @@
 
         <script language="JavaScript">
 
-            var exampleSocket = new WebSocket("ws://47.95.205.248:9501");
+            var exampleSocket = new WebSocket("ws://192.168.1.99:9501");
 
             exampleSocket.onmessage = function (event) {
                 console.log(event.data);

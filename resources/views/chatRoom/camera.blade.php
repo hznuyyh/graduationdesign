ad1fe800c90559c890016d93219d41ead423c8bb
hznuyyh <623936780@qq.com>
2018-02-26 20:48:29 +0800

    updatre

--- resources/views/chatRoom/camera.blade.php
+++ resources/views/chatRoom/camera.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
     @section('content')
-       <iframe src="/camera.html" width="500" height="500">
+       <iframe src="http://127.0.0.1:8000/camera.html" width="500" height="500">
 
        </iframe>
     @endsection

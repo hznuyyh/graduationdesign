78ef5c39cef5ffd6383179c267aaaf40675e9b2a
hznuyyh <623936780@qq.com>
2018-03-27 11:00:03 +0800

    走360镜像

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <title>计算机图形学</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.useso.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Styles -->

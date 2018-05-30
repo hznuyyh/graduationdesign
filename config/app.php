977e13a0941a8016d3581bd66ae390b67a3d5d7b
hznuyyh <623936780@qq.com>
2018-05-30 11:58:08 +0800

    update

--- config/app.php
+++ config/app.php
@@ -177,8 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Overtrue\LaravelUEditor\UEditorServiceProvider::class,
-        FRLog\Log::class
-
     ],
 
     /*
@@ -227,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'FrLog' => FRLog\Log::class
     ],
 
 ];
